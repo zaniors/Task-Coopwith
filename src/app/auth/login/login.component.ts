a1f2d01cc33bf608558308c2c3849388c542a8ff
zhangxu <2590000714@qq.com>
2018-04-04 19:47:20 +0800

    登录组件添加表单验证

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'coopwith-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'coopwith-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+    loginForm: FormGroup;
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.loginForm = new FormGroup({
+            email: new FormControl('932034728@qq.com', Validators.compose([Validators.required, Validators.email])),
+            password: new FormControl('', Validators.required)
+        })
+    }
+
+    onSubmit(formData: NgForm, ev: Event): void {
+        ev.preventDefault();
+        console.log(formData);
+    }
 
 }
