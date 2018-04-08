be7720ea296d19cbd7e50a43fb6b61503d7fed0b
zhangxu <2590000714@qq.com>
2018-04-08 23:09:20 +0800

    添加本地数据库，利用json-server开启建立本地数据库和API

---
+++ src/app/app.config.ts
@@ -0,0 +1,5 @@
+import { Appconfig } from "./shared/interface/appconfig";
+
+export const APP_DI_CONFIG: Appconfig = {
+    appBaseUrl: 'http://localhost:5201',
+};
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -8,7 +8,31 @@ import { LoadSvgResources } from '../utils/svg.util';
 import { AppCommonModule } from '../shared/common/common.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutes } from '../app.routing';
+import 'rxjs/add/observable/from';
+import 'rxjs/add/observable/concat';
+import 'rxjs/add/observable/zip';
+import 'rxjs/add/observable/range';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/merge';
+import 'rxjs/add/observable/combineLatest';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mapTo';
+import 'rxjs/add/operator/pluck';
+import 'rxjs/add/operator/defaultIfEmpty';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/reduce';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/withLatestFrom';
+import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/take';
+import 'rxjs/add/operator/count';
+import 'rxjs/add/operator/do';
+import { ServicesModule } from '../shared/service/services.module';
+import { APP_DI_CONFIG } from '../app.config';
 
 @NgModule({
     imports: [
@@ -17,11 +41,18 @@ import 'rxjs/add/operator/take';
         AppCommonModule,
         UtilsModule,
         AppRoutes,
+        ServicesModule.forRoot(),
     ],
     declarations: [
         HeaderComponent,
         FooterComponent,
         SidebarComponent
+    ],
+    providers: [
+        {
+            provide: 'BASE_CONFIG',
+            useValue: APP_DI_CONFIG
+        }
     ],
     exports: [
         HeaderComponent,
